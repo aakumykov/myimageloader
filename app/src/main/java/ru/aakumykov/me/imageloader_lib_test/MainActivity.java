19ba7ab296347bb70522e3b9f34d5d887d348479
Андрей Кумыков <aakumykov@yandex.ru>
2019-05-09 17:54:57 +0700

    1st

---
+++ app/src/main/java/ru/aakumykov/me/imageloader_lib_test/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.aakumykov.me.imageloader_lib_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
