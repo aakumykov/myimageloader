523b51bc353072235bbb1d69992538ff19d3a480
Андрей Кумыков <aakumykov@yandex.ru>
2019-05-09 18:18:51 +0700

    Utils с доступом package-private.

--- myimageloader/src/main/java/ru/aakumykov/me/myimageloader/Utils.java
+++ myimageloader/src/main/java/ru/aakumykov/me/myimageloader/Utils.java
@@ -2,14 +2,14 @@ package ru.aakumykov.me.myimageloader;
 
 import android.view.View;
 
-public class Utils {
+class Utils {
     private Utils(){}
 
-    public static void show(View view) {
+    static void show(View view) {
         view.setVisibility(View.VISIBLE);
     }
 
-    public static void hide(View view) {
+    static void hide(View view) {
         view.setVisibility(View.GONE);
     }
 
