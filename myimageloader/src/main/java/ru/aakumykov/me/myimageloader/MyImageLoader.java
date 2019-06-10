df4f4a73a448c4eb51e3bcf8aec526336975fbd2
Андрей Кумыков <aakumykov@yandex.ru>
2019-06-10 13:55:20 +0700

    Проверка аргументов с выбросом исключнеий.

--- myimageloader/src/main/java/ru/aakumykov/me/myimageloader/MyImageLoader.java
+++ myimageloader/src/main/java/ru/aakumykov/me/myimageloader/MyImageLoader.java
@@ -2,6 +2,7 @@ package ru.aakumykov.me.myimageloader;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -20,44 +21,62 @@ public class MyImageLoader {
             final ViewGroup imageContainer,
             String imageURL,
             int errorPlaceholderResourceId
-//            ,LoadImageToContainerCallbacls callbacls
     ) {
+        // Проверяю аргументы, кроме imageURL.
+        if (null == context)
+            throw new IllegalArgumentException("There is no context supplied.");
+
+        if (null == imageContainer)
+            throw new IllegalArgumentException("There is no image container supplied.");
+
+        if (errorPlaceholderResourceId <= 0) {
+            throw new IllegalArgumentException("Illegal errorPlaceholder resourceId");
+        }
+
+        // Готовлю программный ImageView и errorDrawable
         ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
         );
 
-        ProgressBar progressBar = new ProgressBar(context);
-        progressBar.setLayoutParams(layoutParams);
-
         final ImageView imageView = new ImageView(context);
-        imageView.setLayoutParams(layoutParams);
-        imageView.setAdjustViewBounds(true);
+                        imageView.setLayoutParams(layoutParams);
+                        imageView.setAdjustViewBounds(true);
 
         final Drawable errorDrawable = context.getResources().getDrawable(errorPlaceholderResourceId);
 
+        // Проверяю imageURL
+        if (TextUtils.isEmpty(imageURL)) {
+            dispalyImageResource(imageContainer, imageView, errorDrawable);
+            return;
+        }
+
+        // Показываю крутилку ожидания изображения
+        ProgressBar progressBar = new ProgressBar(context);
+        progressBar.setLayoutParams(layoutParams);
         imageContainer.addView(progressBar);
         Utils.show(imageContainer);
 
+        // Загружаю картинку
         Glide.with(context)
                 .load(imageURL)
                 //.diskCacheStrategy(DiskCacheStrategy.NONE)
                 .into(new CustomTarget<Drawable>() {
                     @Override
                     public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                        imageView.setImageDrawable(resource);
-                        imageContainer.removeViewAt(0);
-                        imageContainer.addView(imageView);
+                        dispalyImageResource(imageContainer, imageView, resource);
                     }
 
                     @Override
                     public void onLoadCleared(@Nullable Drawable placeholder) {
-                        imageView.setImageDrawable(errorDrawable);
-                        imageContainer.removeViewAt(0);
-                        imageContainer.addView(imageView);
+                        dispalyImageResource(imageContainer, imageView, errorDrawable);
                     }
                 });
     }
 
-
+    private static void dispalyImageResource(ViewGroup imageContainer, ImageView imageView, Drawable imageResource) {
+        imageView.setImageDrawable(imageResource);
+        imageContainer.removeViewAt(0);
+        imageContainer.addView(imageView);
+    }
 }
